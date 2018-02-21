2d643cd290f604fa6723fda6e90a355904780b46
nitin.singh <nitin.singh@globallogic.com>
2018-02-20 23:52:15 -0600

    test

--- src/test/java/foo/AppTest.java
+++ src/test/java/foo/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest{
 		
 		
 	
-		System.out.println("Inside loginpage");
+		System.out.println("Inside loginpage  ");
 		
 		Assert.assertEquals(driver.getTitle(), "Google");
 	}
