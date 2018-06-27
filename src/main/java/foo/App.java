8d94df2ae796e289a471d58d10b61b05892216f2
autofusion <31687556+autofusion@users.noreply.github.com>
2018-06-27 11:51:12 +0530

    Update App.java

--- src/main/java/foo/App.java
+++ src/main/java/foo/App.java
@@ -34,7 +34,7 @@ public int  deleteAccount(int a,int b){
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello all to latest code worlds...Test Anand...27-June...!" );
+        System.out.println( "Hello all to latest code worlds......27-June...2!" );
         
     }
 }
